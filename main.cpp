9ba559d6b91d80194d7bfd22309b6ce745bc0819
MascaChapas27 <angelmartinez2003@hotmail.es>
2023-06-03 21:22:12 +0200

    textbox glitches now glitch the original text instead of creating a random glitch

--- main.cpp
+++ main.cpp
@@ -6,11 +6,9 @@ int main(){
     gabrielaSpeakingSounds[0] = "sounds\\speaking\\gabriela\\gabrielaSpeaking0.wav";
     gabrielaSpeakingSounds[1] = "sounds\\speaking\\gabriela\\gabrielaSpeaking1.wav";
     gabrielaSpeakingSounds[2] = "sounds\\speaking\\gabriela\\gabrielaSpeaking2.wav";
-    Textbox t1(GABRIELA,PLAYER,"sprites\\textbox\\gabrielaTextbox.png",gabrielaSpeakingSounds,"fonts\\gabriela.ttf", false);
-    Textbox t2(GABRIELA,PLAYER,"sprites\\textbox\\gabrielaTextbox.png",gabrielaSpeakingSounds,"fonts\\gabriela.ttf", true);
+    Textbox t1(GABRIELA,PLAYER,"sprites\\textbox\\gabrielaTextbox.png",gabrielaSpeakingSounds,"fonts\\gabriela.ttf", true);
     t1.setText("amai la kuki xdxdxd\nsi o que eh kuki");
-    t2.setText("que dices");
-    while(t1.update() && t2.update()){
+    while(t1.update()){
 
     }
 }

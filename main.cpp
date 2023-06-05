4f3389f53cb6668e52b93d3f41ee7a0ac5f9d975
MascaChapas27 <angelmartinez2003@hotmail.es>
2023-06-05 16:15:20 +0200

    only one speaking sfx is needed (pitch changes do the trick)

--- main.cpp
+++ main.cpp
@@ -2,14 +2,11 @@
 #include <thread>
 #include <chrono>
 int main(){
-    std::string gabrielaSpeakingSounds[3];
-    gabrielaSpeakingSounds[0] = "sounds\\speaking\\gabriela\\gabrielaSpeaking0.wav";
-    gabrielaSpeakingSounds[1] = "sounds\\speaking\\gabriela\\gabrielaSpeaking1.wav";
-    gabrielaSpeakingSounds[2] = "sounds\\speaking\\gabriela\\gabrielaSpeaking2.wav";
-    Textbox t1(GABRIELA,PLAYER,"sprites\\textbox\\gabrielaTextbox.png",gabrielaSpeakingSounds,"fonts\\gabriela.ttf", true);
+    Textbox t1(GABRIELA,PLAYER,"sprites\\textbox\\gabrielaTextbox.png","sounds\\speaking\\gabrielaSpeaking.wav","fonts\\gabriela.ttf","sounds\\speaking\\glitch0.wav");
     t1.setText("amai la kuki xdxdxd\nsi o que eh kuki");
-    Textbox t2(GABRIELA,PLAYER,"sprites\\textbox\\gabrielaTextbox.png",gabrielaSpeakingSounds,"fonts\\gabriela.ttf", false);
+    Textbox t2(GABRIELA,PLAYER,"sprites\\textbox\\gabrielaTextbox.png","sounds\\speaking\\gabrielaSpeaking.wav","fonts\\gabriela.ttf");
     t2.setText("que dices");
+
     while(t1.update() && t2.update()){
 
     }

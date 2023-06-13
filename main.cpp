f07da02631ae5adbe8aac54a7f7f19d585e3ab33
MascaChapas27 <angelmartinez2003@hotmail.es>
2023-06-13 22:07:39 +0200

    oh god yes it works for linux i just had to change the \ to / when specifying a file path

--- main.cpp
+++ main.cpp
@@ -7,29 +7,29 @@ int main(){
     std::cout << "Loading textures..." << std::endl;
     // All textures are initialized
     ResourceHolder<sf::Texture,TextureID> textureHolder;
-    textureHolder.load(GabrielaTextbox,"sprites\\textbox\\gabrielaTextbox.png");
-    textureHolder.load(DanielaTextbox,"sprites\\textbox\\danielaTextbox.png");
-    textureHolder.load(BystanderTextbox,"sprites\\textbox\\bystanderTextbox.png");
+    textureHolder.load(GabrielaTextbox,"sprites/textbox/gabrielaTextbox.png");
+    textureHolder.load(DanielaTextbox,"sprites/textbox/danielaTextbox.png");
+    textureHolder.load(BystanderTextbox,"sprites/textbox/bystanderTextbox.png");
 
     std::cout << "Loading sound effects..." << std::endl;
     // All sound buffers are initialized
     ResourceHolder<sf::SoundBuffer,SoundID> soundHolder;
-    soundHolder.load(GabrielaSpeaking,"sounds\\speaking\\gabrielaSpeaking.wav");
-    soundHolder.load(DanielaSpeaking,"sounds\\speaking\\danielaSpeaking.wav");
-    soundHolder.load(Glitch0,"sounds\\speaking\\glitch0.wav");
-    soundHolder.load(Glitch1,"sounds\\speaking\\glitch1.wav");
-    soundHolder.load(AdvanceConversation,"sounds\\speaking\\advanceConversation.wav");
+    soundHolder.load(GabrielaSpeaking,"sounds/speaking/gabrielaSpeaking.wav");
+    soundHolder.load(DanielaSpeaking,"sounds/speaking/danielaSpeaking.wav");
+    soundHolder.load(Glitch0,"sounds/speaking/glitch0.wav");
+    soundHolder.load(Glitch1,"sounds/speaking/glitch1.wav");
+    soundHolder.load(AdvanceConversation,"sounds/speaking/advanceConversation.wav");
 
     std::cout << "Loading fonts..." << std::endl;
     // All fonts are initialized
     ResourceHolder<sf::Font,FontID> fontHolder;
-    fontHolder.load(Gabriela,"fonts\\gabriela.ttf");
-    fontHolder.load(Daniela,"fonts\\daniela.ttf");
+    fontHolder.load(Gabriela,"fonts/gabriela.ttf");
+    fontHolder.load(Daniela,"fonts/daniela.ttf");
 
     std::cout << "Loading conversations..." << std::endl;
     // All conversations are initialized
     ConversationHolder conversationHolder(soundHolder);
-    conversationHolder.load("files\\Conversations.txt");
+    conversationHolder.load("files/Conversations.txt");
 
     int codigo = 0;
 
